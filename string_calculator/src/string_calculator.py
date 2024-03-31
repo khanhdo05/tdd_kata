dddb88bdbee2c1102773f6a7d7f0c10cbe670326
khoai <khanhphuongdo28@gmail.com>
2024-03-31 11:04:33 -0600

    Step 1 Stage 3

--- string_calculator/src/string_calculator.py
+++ string_calculator/src/string_calculator.py
@@ -1,6 +1,9 @@
 class StringCalculator():
     def Add(string):
+        result = 0
         if len(string) == 0:
             return 0
-        if len(string) == 1:
-            return int(string)
+        list_of_char = string.split(",")
+        for char in list_of_char:
+            result += int(char)
+        return result
