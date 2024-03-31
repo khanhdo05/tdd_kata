0a2ed63b25fb55390e6f6c4c80be6af85525c5a6
khoai <khanhphuongdo28@gmail.com>
2024-03-31 11:07:22 -0600

    Step 2

--- string_calculator/test/test_string_calculator.py
+++ string_calculator/test/test_string_calculator.py
@@ -2,6 +2,7 @@ import pytest
 from src.string_calculator import *
 
 # Batch 1: A simple String calculator with the below method signature that takes in a string of up to two comma seperated numbers and returns the sum of those numbers.
+# Delimiter: ,
 
 def test_empty_string_gets_zero():
     assert StringCalculator.Add("") == 0
@@ -9,5 +10,14 @@ def test_empty_string_gets_zero():
 def test_3_gets_3():
     assert StringCalculator.Add("3") == 3
 
-def test_11_and_5_gets_16():
+def test_11_5_gets_16():
     assert StringCalculator.Add("11,5") == 16
+
+# Batch 2: Allow the Add method to handle an unknown amount of numbers
+# Delimiter: ,
+
+def test_1_2_3_gets_6():
+    assert StringCalculator.Add("1,2,3")
+
+def test_larger_input_gets_correct_sum():
+    assert StringCalculator.Add("1,2,3,9,3,4,2,4,3,3") == 34
