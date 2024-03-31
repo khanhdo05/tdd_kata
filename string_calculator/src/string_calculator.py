8968d76e8dcca8487f7b54443c091877ecb5e66f
khoai <khanhphuongdo28@gmail.com>
2024-03-31 11:09:12 -0600

    add comment - refactor

--- string_calculator/src/string_calculator.py
+++ string_calculator/src/string_calculator.py
@@ -1,9 +1,16 @@
 class StringCalculator():
     def Add(string):
         result = 0
+        
+        # When receive empty string, sum is 0
         if len(string) == 0:
             return 0
+        
+        # Split the string into a list of characters by comma as the delimiter
         list_of_char = string.split(",")
+        
+        # Iterate through the list, convert char -> int, and add up to the sum
         for char in list_of_char:
             result += int(char)
+
         return result
